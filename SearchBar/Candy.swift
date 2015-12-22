77fe4c632e6caf6bdc9525160d163271d231e67a
Edward P. Legaspi <czetsuya@gmail.com>
2015-12-22 12:40:58 +0800

    Origin

---
+++ SearchBar/Candy.swift
@@ -0,0 +1,19 @@
+//
+//  Car.swift
+//  SearchBar
+//
+//  Created by Edward P. Legaspi on 17/12/2015.
+//  Copyright © 2015 Brood Camp. All rights reserved.
+//
+
+
+class Candy {
+    var category : String
+    var name : String
+    
+    init(category : String, name : String){
+        self.category = category
+        self.name = name
+    }
+    
+}
--- SearchBar/ViewController.swift
+++ SearchBar/ViewController.swift
@@ -6,20 +6,87 @@
 //  Copyright © 2015 Brood Camp. All rights reserved.
 //
 
+
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UISearchResultsUpdating {
+    
+    
+    
+    var candies = [Candy]()
+    
+    var filteredCandies = [Candy]()
+    
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        candies = [
+            Candy(category:"One", name:"LETTER A"),
+            Candy(category:"TWO", name:"LETTER B"),
+        ]
+       
+        
+        
+        searchController.searchResultsUpdater = self
+        searchController.dimsBackgroundDuringPresentation = false
+        definesPresentationContext = true
+        tableView.tableHeaderView = searchController.searchBar
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+       return 1
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if searchController.active && searchController.searchBar.text != "" {
+            return filteredCandies.count
+        }else{
+            return candies.count
+        }
+ 
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+        
+        let candy: Candy
+        
+        if searchController.active && searchController.searchBar.text != "" {
+            candy = filteredCandies[indexPath.row]
+        } else {
+            candy = candies[indexPath.row]
+        }
+        
+        cell.textLabel?.text = candy.name
+        cell.detailTextLabel?.text = candy.category
+        
+        return cell
+    }
+    
+    
 
-
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+        filterContentForSearchText(searchController.searchBar.text!)
+    }
+    
+    func filterContentForSearchText(searchText: String, scope: String = "All") {
+        filteredCandies = candies.filter { candy in
+            return candy.name.lowercaseString.containsString(searchText.lowercaseString) || candy.category.lowercaseString.containsString(searchText.lowercaseString)
+        }
+        
+        tableView.reloadData()
+    }
+    
+    
 }
 
